b771ad630d02d375b2a9d5dfadb79572a1ee4f26
Anton <balaevantoine@gmail.com>
2021-06-12 01:45:40 +0300

    smthg

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,10 +1,11 @@
-import React from 'react'
-import { Header } from '@sberdevices/plasma-ui';
+import React, { useState }from 'react'
+import { Header, Button, Container } from '@sberdevices/plasma-ui';
+import { IconHelp } from '@sberdevices/plasma-icons';
 
 export const Head = () => {
-    return ( 
-        <Header title = "Финансовый трекер"
-                logo = "./images/logo"
-                logoAlt = "Logo" />
-    )
+  return (
+    <Header title = "Финансовый трекер" logo = "./images/logo.png" logoAlt = "Logo">
+        <Button text="Помощь" contentRight={<IconHelp size="xs"/>}/>
+    </Header>
+  )
 }
