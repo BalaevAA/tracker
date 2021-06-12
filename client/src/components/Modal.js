4b86090bbf5c331bba8c34c23730341558cc7d16
Anton <balaevantoine@gmail.com>
2021-06-12 04:04:03 +0300

    last chance

---
+++ client/src/components/Modal.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+export const Modal = (props) => {
+
+    return (
+        <div>
+            <div className={`modal__wrapper ${props.isOpened ? 'open' : 'close'}`} style={{...props.style}}>
+                <div className='modal__body'>
+                    <div className='modal__close' onClick={props.onModalClose}>×</div>
+                    <h2>{props.title}</h2>
+                    <hr/>
+                    {props.children}
+                </div>
+            </div>
+        </div>
+    )
+}
